13aa7d3242be58e76344c4ce56888d6755d2103c
kannan020787 <kannan.vmsc@gmail.com>
2019-12-06 16:26:40 +0530

    Code added to the repository

---
+++ src/add/AdressCheck.java
@@ -0,0 +1,11 @@
+package add;
+
+public class AdressCheck {
+public static void main(String[] args) {
+	String s=("5-35-2a,venkateshnivas,Aruppukottai");
+	boolean a =s.contains("636402");
+	System.out.println(a);
+}	
+	
+}
+
